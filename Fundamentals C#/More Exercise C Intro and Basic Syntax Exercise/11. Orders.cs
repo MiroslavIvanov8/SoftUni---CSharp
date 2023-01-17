a100503e600559500b5298bdb7f22b5aea83a79c
MiroslavIvanov8 <litiers3@gmail.com>
2023-01-17 22:45:03 +0200

    Solutions to Basic Syntax, Cond State and Loops tasks
    my first Exercise Fundamentals Module all tasks complete

---
+++ Fundamentals C#/More Exercise C Intro and Basic Syntax Exercise/11. Orders.cs
@@ -0,0 +1,29 @@
+﻿
+
+// ((daysInMonth * capsulesCount) * pricePerCapsule)
+
+
+
+double numberOfOrders = double.Parse(Console.ReadLine());
+double daysInMonth = 0;
+double capsulesCount = 0;
+double pricePerCapsule =0;
+
+double price = 0;
+double total = 0;
+
+for (int i = 0; i < numberOfOrders; i++)
+{
+    
+    pricePerCapsule = double.Parse(Console.ReadLine());
+    daysInMonth =double.Parse(Console.ReadLine());
+    capsulesCount =double.Parse(Console.ReadLine());
+
+    price = (daysInMonth* capsulesCount)* pricePerCapsule;
+    total += price;
+
+    Console.WriteLine($"The price for the coffee is: ${price:f2}");
+    price = 0;
+}
+
+Console.WriteLine($"Total: ${total:f2}");
