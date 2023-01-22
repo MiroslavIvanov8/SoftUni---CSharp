8724a5102eb12baa57fc9a875f136049a2969608
MiroslavIvanov8 <litiers3@gmail.com>
2023-01-22 23:36:38 +0200

    been practicing some array functions

--- Fundamentals C#/Data types and Variables/Data Types And Variables More Exercise/05.Decrypting Massage/05.Decrypting Massage.cs
+++ Fundamentals C#/Data types and Variables/Data Types And Variables More Exercise/05.Decrypting Massage/05.Decrypting Massage.cs
@@ -1,16 +1,28 @@
 ﻿int key = int.Parse(Console.ReadLine());
 int lines = int.Parse(Console.ReadLine());
-
+char[] arr = new char[lines];
 for (int i = 0; i < lines; i++)
 {
     char symbol = char.Parse(Console.ReadLine());
     int letter = symbol + key;
-    Console.Write($"{(char)letter}");
+    //Console.Write($"{(char)letter}");
+    arr[i]+=(char)letter;
 }
-    
-      
-    
 
+//foreach (char ch in arr)
+//{
+//    Console.Write("{0}", ch);
+//}
+for (int i = 0; i < arr.Length; i++)
+{
     
+    Console.Write(arr[i]);
+}
+
+
+
+
+
+
 
 
