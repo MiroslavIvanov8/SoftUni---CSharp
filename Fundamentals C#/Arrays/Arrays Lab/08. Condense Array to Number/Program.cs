e194ca8a059de3aa00e0a84096ab93eaa9a75320
MiroslavIvanov8 <litiers3@gmail.com>
2023-02-02 20:06:51 +0200

    got 100/100 on the last task. so happy !

--- Fundamentals C#/Arrays/Arrays Lab/08. Condense Array to Number/Program.cs
+++ Fundamentals C#/Arrays/Arrays Lab/08. Condense Array to Number/Program.cs
@@ -1,14 +1,15 @@
 ﻿int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-int[] condensedArr = new int [0];
+int[] condensedArr = new int [arr.Length];
 int sum = 0;
-for (int i = 0; i < arr.Length - 1; i++)
+for (int i = 0; i < arr.Length-1; i++)
 {
-    condensedArr = arr;
-    //condensedArr[i] = arr[i] + arr[i + 1];
+    condensedArr = arr;    
     for (int j = 0; j < condensedArr.Length - 1-i; j++)
     {
         condensedArr[j]= condensedArr[j] + condensedArr[j + 1];
     }
     condensedArr = arr;
 }
+if(arr.Length == 1)
+ condensedArr = arr;
 Console.WriteLine(condensedArr[0]);
