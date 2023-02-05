9f1b95d729cc250742ab413e5e6b742e62fc53b1
MiroslavIvanov8 <litiers3@gmail.com>
2023-02-05 09:43:30 +0200

    some more problem solutions

---
+++ Fundamentals C#/Methods/Methods Exercise/11. Array Manipulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11._Array_Manipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Fundamentals C#/Methods/Methods Exercise/9. Palindrome Integers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _9._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
