63512ce571aefd394d5cea4bc93b60995ba1c8aa
MiroslavIvanov8 <litiers3@gmail.com>
2023-02-05 18:21:16 +0200

    arrays ex uploaded

---
+++ Fundamentals C#/Arrays/Arrays Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+
+
+
+int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = int.Parse(Console.ReadLine());
+int firstNum = inputNumbers[0];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < inputNumbers.Length - 1; j++)
+    {
+        inputNumbers[j] = inputNumbers[j+1];
+    }
+    inputNumbers[inputNumbers.Length-1] = firstNum;
+    firstNum = inputNumbers[0];
+}
+Console.WriteLine(string.Join(" ", inputNumbers));
---
+++ Fundamentals C#/Arrays/Arrays Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray(); //1 1 1 2 3 1 3 3
+int sequence = 0;
+int BestSequence = 0;
+int sequenceNumber = 0;
+int bestNumber = 0;
+for (int i = 0; i < inputArray.Length-1; i++)
+{
+    if (inputArray[i] != inputArray[i + 1])
+    {
+        sequence = 0;
+        continue;
+    }
+    if (inputArray[i] == inputArray[i + 1])
+    {
+        sequence++;
+        sequenceNumber = inputArray[i];
+    }
+
+    if (sequence > BestSequence)
+    {
+        BestSequence = sequence;
+        bestNumber = inputArray[i];
+    }
+
+    
+}
+
+for (int i = 0; i <= BestSequence; i++)
+{
+    Console.Write(bestNumber+ " ");
+}
