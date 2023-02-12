520fe47b98335b64cfe57a100c70f763588d5adb
MiroslavIvanov8 <litiers3@gmail.com>
2023-02-13 00:55:41 +0200

    Some mid exam problem solutions
    mid exam solutions and some list exercise problems solved

---
+++ Fundamentals C#/Lists/Lists Exercise/01. Train/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> wagons = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            while(input != "end")
+            {
+                //•	Add {passengers} – add a wagon to the end of the train with the given number of passengers.
+                //•passengers} – find a single wagon to fit all the incoming passengers(starting from the first wagon).
+                string [] command = input.Split(" ");
+
+                if (command[0] == "Add")
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(command[0]);
+                    //int wagon= wagons.First(x => x + passengers <= maxCapacity);
+                    //wagons[wagons.IndexOf(wagon)] += passengers;
+                       
+                    //foreach(var wagon in wagons)
+                    //{
+                    //    if (wagons[wagon] + passengers <= maxCapacity)
+                    //    {
+                    //        wagons[wagons.IndexOf(wagon)] += passengers;
+                    //        break;
+                    //    }
+                    //}
+
+                   for(int i =0;i<wagons.Count;i++)
+                   {
+                       if (wagons[i] + passengers <= maxCapacity)
+                       {
+                           wagons[i] += passengers;
+                           break;
+                       }
+                   }
+                }
+
+                input = Console.ReadLine(); 
+                
+            }
+            
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ Fundamentals C#/Mid Exams/01.Mid Exam Retake/The Lift/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInQue = int.Parse(Console.ReadLine());
+            int[] wagons =Console.ReadLine()
+                                 .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+            int freeSeat = 0;
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                if (wagons[i] < 4)
+                {
+                    freeSeat = 4 - wagons[i]; // free seats in each wagon
+                                              // need to check if there are enough people \
+                    if (freeSeat <= peopleInQue)
+                    {
+                        wagons[i] += freeSeat;
+                        peopleInQue -= freeSeat;
+                    }
+                    // if free seats > people 
+                    else 
+                    {
+                        freeSeat = peopleInQue;
+                        wagons[i] += freeSeat;
+                        peopleInQue-= freeSeat;
+                    }
+                    
+                }
+                if(peopleInQue==0)
+                {
+                    break;
+                }
+            }
+            bool allTaken = true;
+            if (peopleInQue == 0)
+            { 
+                for(int i=0;i<wagons.Length;i++)
+                {
+                    if (wagons[i] < 4)
+                    {
+                        Console.WriteLine("The lift has empty spots!");
+                        Console.WriteLine(string.Join(" ", wagons));
+                        allTaken= false;
+                        break;
+                    }
+                }
+            }
+            else if (peopleInQue > 0)
+            {    allTaken = false;
+                Console.WriteLine($"There isn't enough space! {peopleInQue} people in a queue!");
+                Console.WriteLine(string.Join(" ",wagons));
+            }
+            if(peopleInQue==0 && allTaken)
+                Console.WriteLine(string.Join(" ",wagons));
+        }
+    }
+}
