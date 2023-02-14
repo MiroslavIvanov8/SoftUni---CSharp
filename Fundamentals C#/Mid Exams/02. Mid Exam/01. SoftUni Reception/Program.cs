b415e419bcc6e8ee29215b7444367df85d23f86e
MiroslavIvanov8 <litiers3@gmail.com>
2023-02-14 22:43:54 +0200

    want to push so i destroy the upcomming exam

---
+++ Fundamentals C#/Mid Exams/02. Mid Exam/01. SoftUni Reception/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int employee1 = int.Parse(Console.ReadLine());
+            int employee2 = int.Parse(Console.ReadLine());
+            int employee3 = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            int employeesUnite = employee1+ employee2 + employee3;
+            int hourCounter = 0;
+
+            while(students>=0) 
+            {
+                hourCounter++;                
+                if (hourCounter % 4 == 0)
+                {                   
+                    continue;
+                }
+                if (students - employeesUnite < 0)
+                    break;
+                students-= employeesUnite;
+
+                
+
+            }
+            Console.WriteLine($"Time needed: {hourCounter}h.");
+        }
+    }
+}
