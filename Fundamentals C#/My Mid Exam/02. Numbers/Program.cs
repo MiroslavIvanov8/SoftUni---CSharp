be3d48f08be593fbbf53a535acf1ffac6cb0af46
MiroslavIvanov8 <litiers3@gmail.com>
2023-02-19 15:02:37 +0200

    My Mid Exam Fundamentals
    100/100, even played a little bit to put to use some methods in order to make it more interesting :)

---
+++ Fundamentals C#/My Mid Exam/02. Numbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace _02._Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            string input;
+            while ((input = Console.ReadLine()) != "Finish")
+            {
+                string[] commandArgs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string commanType = commandArgs[0];
+                if (commanType == "Add")
+                {
+                    int value = int.Parse(commandArgs[1]);
+                    numbers.Add(value);
+                }
+                else if (commanType == "Remove")
+                {
+                    int value = int.Parse(commandArgs[1]);
+                    numbers.Remove(value);
+                }
+                else if (commanType == "Replace")
+                {                                                 
+                    int value = int.Parse(commandArgs[1]);                        // 0  1  2  3  4  5
+                    if (numbers.Contains(value))                                 //  5  9 70 -56 9  9 
+                    {
+                        int replacement = int.Parse(commandArgs[2]);
+                        int index = numbers.IndexOf(value);
+                        numbers.RemoveAt(index);
+                        numbers.Insert(index, replacement);
+                    }
+                }
+                else if (commanType == "Collapse")
+                {
+                    int value = int.Parse(commandArgs[1]);
+                    numbers.RemoveAll(x => x < value);
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Fundamentals C#/My Mid Exam/03. Chat Logger/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _03._Chat_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chat = new List<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] commandArgs = input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                string commandType = commandArgs[0];
+                if (commandType == "Chat")
+                {
+                    string massage = commandArgs[1];
+                    AddMasssage(chat, massage);
+                }
+                else if (commandType == "Delete")
+                {
+                    string delete = commandArgs[1];
+                    RemoveMassage(chat, delete);
+                }
+                else if (commandType == "Edit")
+                {
+                    string massage = commandArgs[1];
+                    string editedMassage = commandArgs[2];
+                    int index = chat.IndexOf(massage);
+                    if (index < 0 && index > chat.Count - 1)
+                    {
+                        continue;
+                    }
+                    else
+                        EditMassage(chat, index, editedMassage);
+                }
+                else if (commandType=="Pin")
+                {
+                    PinMassage(chat, commandArgs);
+                }
+                else if (commandType== "Spam")
+                {
+                    for (int i = 1; i < commandArgs.Length; i++)
+                    {
+                        chat.Add(commandArgs[i]);
+                    }
+                }
+                
+            }
+            Console.WriteLine(string.Join(Environment.NewLine,chat));
+        }
+        static List<string> AddMasssage(List<string> chat, string massage)
+        {
+            chat.Add(massage);
+            return chat;
+        }
+        static List<string> RemoveMassage(List<string> chat, string delete)
+        {
+            chat.Remove(delete);
+            return chat;
+        }
+        static List<string> EditMassage (List<string> chat, int index,string editedMassage)
+        {
+            chat[index] = editedMassage;
+            return chat;
+        }
+        static List<string> PinMassage(List<string> chat, string[] commandArgs)
+        {
+            string massage = commandArgs[1];
+            int index = chat.IndexOf(massage);
+            chat.Remove(massage);
+            chat.Add(massage);
+            return chat;
+        }
+    }
+}
