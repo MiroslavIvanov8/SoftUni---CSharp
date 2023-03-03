d1db15311f8d5fb850402473751e45ad22bbdb83
MiroslavIvanov8 <litiers3@gmail.com>
2023-03-03 22:33:49 +0200

    exercise
    i want to learn more about assosiative arrays

---
+++ Fundamentals C#/Associative Arrays/02. A Miner Task/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> mining = new Dictionary<string, int>();
+            int counter = 0;
+             
+            string resourse;
+            int quantity = 0;
+            while (true)            
+            {
+                
+                resourse = Console.ReadLine();
+                if (resourse == "stop")
+                    break;
+                quantity = int.Parse(Console.ReadLine());
+
+                if (!mining.ContainsKey(resourse))
+                {
+                    mining.Add(resourse, quantity);
+                }
+                else
+                    mining[resourse] += quantity;
+
+            }
+            foreach (var item in mining)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+        
+    }
+}
