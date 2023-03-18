d232c52e150a47439141dedfded98d8220221792
MiroslavIvanov8 <litiers3@gmail.com>
2023-03-18 18:34:48 +0200

    JESUS!
    i hope i manage to decently learn the materials for the exam. I am aiming for a strong 6. Would really put more time into practising and learning !!

---
+++ Fundamentals C#/Regular Expressions/Regular Expressions Ex/01. Furniture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Security;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> furnitures = new List<string>();
+            string input = string.Empty;
+            double price = 0;
+
+            string pattern = @">>(?<item>[A-z]+)<<(?<price>\d+[.]?[0-9]+)!(?<quantity>\d+)";
+            Regex  regex= new Regex(pattern);
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                bool isMatched = regex.IsMatch(input);
+                if (isMatched)
+                {
+                    Match match = regex.Match(input);
+                    furnitures.Add(match.Groups["item"].Value);
+                    price += double.Parse(match.Groups["price"].Value) * double.Parse(match.Groups["quantity"].Value);
+
+                }
+            }
+            Console.WriteLine($"Bought furniture:");
+            foreach (string furniture in furnitures)
+            {
+                Console.WriteLine(furniture);
+            }
+            Console.WriteLine($"Total money spend: {price:f2}");
+
+        }   
+    }
+}
