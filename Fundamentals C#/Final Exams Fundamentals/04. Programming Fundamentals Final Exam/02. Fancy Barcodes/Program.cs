3ec3152baecda969abc632420f125b09e1252ab2
MiroslavIvanov8 <litiers3@gmail.com>
2023-03-28 16:43:16 +0300

    feeling great 1
    managed to complete these 3 solutions in under 2 hours

---
+++ Fundamentals C#/Final Exams Fundamentals/04. Programming Fundamentals Final Exam/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"^@(#+)(?<product>[A-Z][A-Za-z\d]{4,}[A-Z])@\1$";
+            Regex regex = new Regex(pattern);
+            bool validBarcode = false;
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = regex.Match(input);
+                string currMatch = match.Groups["product"].Value;
+                string productNumber = string.Empty;
+                for (int j = 0; j < currMatch.Length; j++)
+                {
+                    if (Char.IsDigit(currMatch[j]))
+                    {
+                        productNumber += currMatch[j];
+                        validBarcode = true;
+                    }
+                }
+                ;
+                if (productNumber.Length>0)
+                    Console.WriteLine($"Product group: {productNumber}");
+                else if (currMatch.Length == 0)
+                    Console.WriteLine("Invalid barcode");
+                else if (productNumber.Length == 0)
+                    Console.WriteLine("Product group: 00");
+                validBarcode = false;
+
+            }
+        }
+    }
+}
