d4c1acafd86ac4319ab3b6760fbf62955e0b530c
MiroslavIvanov8 <litiers3@gmail.com>
2023-04-22 15:17:43 +0300

    Advanced
    first lab

---
+++ Advanced C#/Stacks and Queues/Stacks and Queues Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _2._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+            string command = string.Empty;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] commandArgs = command.Split();
+                string type = commandArgs[0];
+                if (type == "add")
+                {
+                    stack.Push(int.Parse(commandArgs[1]));
+                    stack.Push(int.Parse(commandArgs[2]));
+
+                }
+                else if (type == "remove")
+                {
+                    int count = int.Parse(commandArgs[1]);
+                    if (stack.Count < count)
+                        continue;
+                    else
+                    {
+                        for(int i =0;i<count;i++)
+                        { 
+                            stack.Pop();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
