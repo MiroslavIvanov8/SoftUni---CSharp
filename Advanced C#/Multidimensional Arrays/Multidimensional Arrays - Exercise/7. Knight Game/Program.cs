67f4851be503d35114cad094e92befc4ce9d4835
MiroslavIvanov8 <litiers3@gmail.com>
2023-05-18 19:23:20 +0300

    кекв
    bombs8 done 90/100

--- Advanced C#/Multidimensional Arrays/Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
+++ Advanced C#/Multidimensional Arrays/Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
@@ -1,42 +1,50 @@
-﻿namespace _7._Knight_Game_2
+﻿using System.Data;
+
+namespace _7._Knight_Game_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int size = int.Parse(Console.ReadLine());
-            char[,] board = new char[size,size];
+            char[,] board = new char[size, size];
+            if (size < 3)
+            {
+                Console.WriteLine(0);
+                return;
+            }
 
             for (int row = 0; row < size; row++)
             {
-                string data = Console.ReadLine();
+                string info = Console.ReadLine();
                 for (int col = 0; col < size; col++)
                 {
-                    board[row, col] = data[col];
+                    board[row, col] = info[col];
                 }
             }
-            
             int knightsRemoved = 0;
-
+            
             while (true)
             {
-                int countMostAttacking = 0;
                 int rowMostAttacking = 0;
                 int colMostAttacking = 0;
+                int countMostAttacking = 0;
                 for (int row = 0; row < size; row++)
                 {
-                    
+
                     for (int col = 0; col < size; col++)
                     {
+                        
+
                         if (board[row, col] == 'K')
                         {
+                            int mostAttacking = MostAttacking(row, col, board, size);
 
-                            int attackedKnights = CountAttackedKnights(row, col, size, board);
-                            if (countMostAttacking < attackedKnights)
+                            if (countMostAttacking < mostAttacking)
                             {
-                                countMostAttacking = attackedKnights;
                                 rowMostAttacking = row;
                                 colMostAttacking = col;
+                                countMostAttacking = mostAttacking;
                             }
                         }
                     }
@@ -49,81 +57,71 @@
                     knightsRemoved++;
                 }
             }
-            Console.WriteLine(knightsRemoved);
-        }
 
-        static int CountAttackedKnights(int row, int col, int size, char[,] board)
+            Console.WriteLine(knightsRemoved);
+
+        }
+        static int MostAttacking(int row, int col, char[,] board, int size)
         {
             int attackedKnights = 0;
-            // horizontal up-left
-            if (IsCellValid(row - 1, col - 2, size))
+            // horizontal-left
+            if (IsCellValid(row - 1, col - 2,size)) 
             {
-                if (board[row - 1, col - 2] == 'K')
-                {
-                    attackedKnights++;
-                }
+                if(board[row - 1, col - 2] =='K')
+                 attackedKnights++; 
             }
-            // horizontal down-left
-            if (IsCellValid(row + 1, col - 2, size))
-            {
-                if (board[row + 1, col - 2] == 'K')
-                {
-                    attackedKnights++;
-                }
-            }
-            // horizontal up-left
-            if (IsCellValid(row - 1, col + 2, size))
-            {
-                if (board[row - 1, col + 2] == 'K')
-                {
-                    attackedKnights++;
-                }
-            }
-            // horizontal down-right
-            if (IsCellValid(row + 1, col + 2, size))
-            {
-                if (board[row + 1, col + 2] == 'K')
-                {
-                    attackedKnights++;
-                }
-            }
-            // vertical up-left
+            // vertical- left
             if (IsCellValid(row - 2, col - 1, size))
             {
                 if (board[row - 2, col - 1] == 'K')
-                {
                     attackedKnights++;
-                }
             }
-            // vertical up-right
+            // horizontal-right
+            if (IsCellValid(row - 1, col + 2, size))
+            {
+                    if (board[row - 1, col + 2] == 'K')
+                        attackedKnights++;
+            }
+            // vertical-right
             if (IsCellValid(row - 2, col + 1, size))
             {
                 if (board[row - 2, col + 1] == 'K')
-                {
                     attackedKnights++;
-                }
             }
-            // vertical down-left
+            // horizontal-left down
+            if (IsCellValid(row + 1, col - 2, size))
+            {
+                if (board[row + 1, col - 2] == 'K')
+                    attackedKnights++;
+            }// vertical-left down
             if (IsCellValid(row + 2, col - 1, size))
             {
                 if (board[row + 2, col - 1] == 'K')
-                {
                     attackedKnights++;
-                }
             }
-            // vertical down-right
+            // horizontal-right down
+            if (IsCellValid(row + 1, col + 2, size))
+            {
+                if (board[row + 1, col + 2] == 'K')
+                    attackedKnights++;
+            }
+            // horizontal-right down
             if (IsCellValid(row + 2, col + 1, size))
             {
                 if (board[row + 2, col + 1] == 'K')
-                {
                     attackedKnights++;
-                }
             }
             return attackedKnights;
+
+
         }
         static bool IsCellValid(int row, int col, int size)
         {
-            return row >= 0 && row < size && col >= 0 && col < size;
+            return
+                row >= 0
+             && row < size
+             && col >= 0
+             && col < size;
         }
     }
 }
---
+++ Advanced C#/Multidimensional Arrays/Multidimensional Arrays - Exercise/8. Bombs1/Program.cs
@@ -0,0 +1,150 @@
+﻿using System.Drawing;
+
+namespace _8._Bombs1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size,size];
+            for (int row = 0; row < size; row++)
+            {
+                int[] rowInfo = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = rowInfo[col];
+                }
+            }
+            string[] bombs = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string bomb in bombs)
+            {
+               int row = bomb[0]-48;
+               int col = bomb[2]-48;
+               int bombStrength = matrix[row, col];
+               matrix = Explosion(row, col, bombStrength ,matrix, size);
+            }
+
+            int aliveCells = AliveCellsCheck(size,matrix);
+            int aliveCellsSum = AliveCellsSum(size, matrix);
+
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {aliveCellsSum}");
+            PrintMatrix(size,matrix);
+
+        }
+        static void PrintMatrix(int size, int[,]matrix)
+        {
+            
+            for (int row = 0; row < size; row++)
+            {
+                
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        private static int AliveCellsSum(int size, int[,] matrix)
+        {
+            int sum = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+            return sum;
+        }
+
+        static int AliveCellsCheck(int size, int[,]matrix)
+        {
+            int aliveCells = 0;
+            
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        aliveCells++;
+                        
+                    }
+                }
+            }
+            return aliveCells;
+           
+        }
+        private static int[,] Explosion(int row, int col, int bombStrength, int[,] matrix, int size)
+        {
+            // horizontal up
+            if (IsCellValid(row-1, col-1,size))
+            {
+               if (matrix[row - 1, col - 1] >0)
+                   matrix[row - 1, col - 1] -= bombStrength;
+            }
+            if (IsCellValid(row - 1, col , size))
+            {
+                if (matrix[row - 1, col] > 0)
+                    matrix[row - 1, col] -= bombStrength;
+            }
+            if (IsCellValid(row - 1, col + 1 , size))
+            {
+                if (matrix[row-1, col + 1] > 0)
+                    matrix[row - 1, col + 1] -= bombStrength;
+            }
+            // left 
+            if (IsCellValid(row , col - 1, size))
+            {
+                if (matrix[row,col - 1] > 0)
+                    matrix[row , col - 1] -= bombStrength;
+            }
+            // right
+            if (IsCellValid(row, col + 1, size))
+            {
+                if (matrix[row, col + 1] > 0)
+                    matrix[row, col + 1] -= bombStrength;
+            }
+            // horizotal down
+            if (IsCellValid(row + 1, col - 1, size))
+            {
+                if (matrix[row + 1,col - 1] > 0)
+                    matrix[row + 1, col - 1] -= bombStrength;
+            }
+            if (IsCellValid(row + 1, col, size))
+            {
+                if (matrix[row + 1,col] > 0)
+                    matrix[row + 1, col] -= bombStrength;
+            }
+            if (IsCellValid(row + 1, col + 1, size))
+            {
+                if (matrix[row + 1,col + 1] > 0)
+                matrix[row + 1, col + 1] -= bombStrength;
+            }
+            matrix[row, col] = 0;
+            return matrix;
+
+        }
+        static bool IsCellValid(int row, int col ,int size)
+        {
+            return row >= 0
+                && row < size
+                && col >= 0
+                && col < size;
+
+        }
+    }
+}
