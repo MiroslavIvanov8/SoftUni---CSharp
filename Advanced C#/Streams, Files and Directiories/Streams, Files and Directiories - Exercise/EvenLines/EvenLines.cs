3f43de58683426c2f226887296060f307c7f5246
MiroslavIvanov8 <litiers3@gmail.com>
2023-05-31 21:57:10 +0300

    missing problems
    more work to be done here 3 more problems

---
+++ Advanced C#/Streams, Files and Directiories/Streams, Files and Directiories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,68 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using StreamReader reader = new(inputFilePath);
+            {
+                StringBuilder sb = new StringBuilder();
+                int count = 0;
+                string line = string.Empty;
+
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+                    
+                    if (count % 2 == 0)
+                    {
+                        string replaceSymbols = ReplaceSymbols(line);
+                        string reversedLine = ReverseLine(replaceSymbols);
+                        sb.AppendLine(reversedLine);
+                    }
+                    count++;
+                }                
+                return sb.ToString();
+            }
+            
+            
+        }
+
+        private static string ReverseLine(string replaceSymbols)
+        {
+            StringBuilder sb = new();
+            string[] text = replaceSymbols.Split(" ",StringSplitOptions.RemoveEmptyEntries) ;
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                sb.Append($"{text[i]} ");
+            }
+            return sb.ToString();
+
+        } 
+
+        private static string ReplaceSymbols(string line)
+        {
+            StringBuilder sb = new StringBuilder(line);
+
+            char[] symbols = { '-', ',','.','!','?' };
+
+            foreach (char ch in symbols)
+            {
+                sb = sb.Replace(ch, '@'); 
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Advanced C#/Streams, Files and Directiories/Streams, Files and Directiories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,67 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+                StringBuilder sb = new StringBuilder();
+                string[] lines = File.ReadAllLines(inputFilePath);
+            int lineNumber = 1;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string line = lines[i];
+                    
+                    int letterCount = CountLetters(line);
+                    int markCount = CountMarks(line);
+
+                    sb.AppendLine($"Line {lineNumber++}:{line} ({letterCount})({markCount})");
+
+                    
+                }
+            File.WriteAllText(outputFilePath,sb.ToString());
+
+        }
+        private static int CountLetters(string line)
+        {
+            string text = line;
+            int count = 0;
+
+            foreach (char ch in text)
+            {
+                if(char.IsLetter(ch))
+                    count++;
+            }
+            return count;
+        }
+        private static int CountMarks(string line)
+        {
+            string text = line;
+            char[] symbols = { '-','?','!','.',',','\''};
+            int count = 0;
+            foreach (char symbol in symbols)
+            {
+                foreach (char ch in text)
+                {
+                    if(symbol==ch)
+                        count++;
+                }
+            }
+            return count;
+            
+        }
+
+        
+    }
+}
