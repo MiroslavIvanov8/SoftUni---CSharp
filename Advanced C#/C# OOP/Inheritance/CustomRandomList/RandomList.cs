414e5f48a98c9cbfb9e1cef79f00a6251cd5f709
MiroslavIvanov8 <litiers3@gmail.com>
2023-06-20 22:09:47 +0300

    le goo

---
+++ Advanced C#/C# OOP/Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        
+        public string RandomString() 
+        {
+            Random random = new Random();
+            int index = random.Next(0, this.Count);
+            string removedValue = this[index];
+            this.RemoveAt(index);
+            return removedValue;
+        }
+    }
+}
