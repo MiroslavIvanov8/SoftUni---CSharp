27adfca8ed8f1e6530eabaabbbec74fd278e7da7
MiroslavIvanov8 <litiers3@gmail.com>
2023-07-05 21:55:51 +0300

    lets goo polymorphism
    i love it
    cant wait to learn more
    hope it doesnt get nasty

---
+++ Advanced C#/C# OOP/Polymorphism/Polymorphism - Lab/Animals/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            FavouriteFood = favouriteFood;
+            Name = name;
+        }
+
+        public string FavouriteFood { get; set; }
+        public string Name { get; set; }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood}";
+        }
+
+    }
+}
