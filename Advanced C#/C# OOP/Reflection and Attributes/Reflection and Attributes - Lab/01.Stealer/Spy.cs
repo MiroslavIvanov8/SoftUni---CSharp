cbfc082a907072c2192757119c441494693284b3
MiroslavIvanov8 <litiers3@gmail.com>
2023-07-15 21:10:50 +0300

    LE GOO

---
+++ Advanced C#/C# OOP/Reflection and Attributes/Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public Spy()
+        {
+
+        }
+
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            FieldInfo[] fields = classType.GetFields((BindingFlags)60);
+
+            object currInstance = Activator.CreateInstance(classType);
+
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine($"Class under investigation: {classType.Name}");
+
+            foreach (FieldInfo field in fields.Where(f=>requestedFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(currInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
