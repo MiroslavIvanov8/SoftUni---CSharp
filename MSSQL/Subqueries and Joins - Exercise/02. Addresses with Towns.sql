6f6bb71f349a32b8c318a2d7797aaa34f5ec7048
MiroslavIvanov8 <litiers3@gmail.com>
2023-10-03 15:43:19 +0300

    4 subquerries ,joins,cte
    4 subquerries ,joins,cte

---
+++ MSSQL/Subqueries and Joins - Exercise/02. Addresses with Towns.sql
@@ -0,0 +1,9 @@
+SELECT top 50
+	e.FirstName
+	,e.LastName
+	,t.[Name] as [Town]
+	,a.AddressText
+FROM Employees AS e
+	JOIN Addresses AS a ON e.AddressID = a.AddressID
+	JOIN Towns AS t ON a.TownID = t.TownID
+ORDER BY e.FirstName,e.LastName
