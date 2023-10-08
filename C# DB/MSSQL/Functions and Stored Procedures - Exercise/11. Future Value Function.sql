d17ebb62cf91c95979236b66fdeab937629249ed
MiroslavIvanov8 <litiers3@gmail.com>
2023-10-08 20:27:55 +0300

    kekw
    taking a break it looks alright for now
    lets see how are the exam preps

---
+++ C# DB/MSSQL/Functions and Stored Procedures - Exercise/11. Future Value Function.sql
@@ -0,0 +1,22 @@
+﻿CREATE OR ALTER FUNCTION ufn_CalculateFutureValue 
+(@Sum DECIMAL(18,4), @YearlyInterestRate FLOAT, @Year INT)
+RETURNS DECIMAL(18,4)
+AS
+BEGIN
+-- FV=I×((1+R)^T)	
+--I  Initial sum
+--R  Yearly interest rate
+--T  Number of years
+DECLARE @cnt INT = 0;
+
+		WHILE @cnt < @Year
+		BEGIN
+		   
+		   SET @Sum *= (1 + @YearlyInterestRate)
+		   SET @cnt += 1
+		END
+
+		RETURN @Sum
+END
+
+
