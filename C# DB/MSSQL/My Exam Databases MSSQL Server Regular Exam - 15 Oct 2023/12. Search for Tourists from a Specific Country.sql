62d4aa665b54bc7bc33f0af834411bcebc4544ac
MiroslavIvanov8 <litiers3@gmail.com>
2023-10-15 11:46:16 +0300

    omegaluk
    last one

---
+++ C# DB/MSSQL/My Exam Databases MSSQL Server Regular Exam - 15 Oct 2023/12. Search for Tourists from a Specific Country.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROC usp_SearchByCountry(@Country VARCHAR(50))
+AS 
+
+SELECT *
+	,COUNT(q.[Name])
+ FROM	(	SELECT 
+			t.[Name]
+			,t.PhoneNumber
+			,t.Email
+				FROM Tourists AS t
+				JOIN Bookings AS b ON t.Id = b.TouristId
+				JOIN Countries AS c ON t.CountryId = c.Id
+		WHERE c.[Name] = @Country
+		) AS q
+GROUP BY q.[Name],q.PhoneNumber, q.Email
