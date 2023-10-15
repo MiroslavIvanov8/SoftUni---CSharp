8aa69aa94bc396b762d5d40f980de30a78163558
MiroslavIvanov8 <litiers3@gmail.com>
2023-10-15 11:44:08 +0300

    kekw
    uploading my exam

---
+++ C# DB/MSSQL/My Exam Databases MSSQL Server Regular Exam - 15 Oct 2023/06. Hotels by Count of Bookings.sql
@@ -0,0 +1,16 @@
+﻿SELECT 
+q.HotelId
+,q.HotelName
+	FROM (
+			SELECT 
+				h.[Name] AS HotelName 
+				,b.Id AS BookingId
+				,h.Id AS HotelId
+			FROM Hotels AS h
+				JOIN HotelsRooms AS hr ON h.Id = hr.HotelId
+				JOIN Rooms AS r ON hr.RoomId = r.Id
+				JOIN Bookings AS b ON h.Id = b.HotelId
+		WHERE r.[Type] = 'VIP Apartment'
+		) AS q
+GROUP BY q.HotelId ,Q.HotelName
+ORDER BY COUNT(q.BookingId) DESC
---
+++ C# DB/MSSQL/My Exam Databases MSSQL Server Regular Exam - 15 Oct 2023/07. Tourists without Bookings.sql
@@ -0,0 +1,8 @@
+﻿SELECT 
+	t.Id
+	,t.[Name]
+	,t.PhoneNumber
+		FROM Tourists AS t
+		LEFT JOIN Bookings AS b ON t.Id = b.TouristId
+	WHERE b.ArrivalDate IS NULL
+ORDER BY t.[Name]
