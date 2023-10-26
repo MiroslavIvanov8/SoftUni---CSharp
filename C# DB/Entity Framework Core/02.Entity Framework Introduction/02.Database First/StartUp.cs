6d25e7b958ea1a67f26079cb3dcf4788693e3f53
MiroslavIvanov8 <litiers3@gmail.com>
2023-10-26 19:28:34 +0300

    crying
    i am gonna lie down and cry

---
+++ C# DB/Entity Framework Core/02.Entity Framework Introduction/02.Database First/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using SoftUni;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var contex = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(contex));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context.Employees
+                .Select(e=> new {e.EmployeeId, e.FirstName,e.MiddleName,e.LastName,departmentName = e.Department.Name, e.Salary})
+                .OrderBy(e=>e.EmployeeId)
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                string[] names = new string[3]{e.FirstName,e.MiddleName, e.FirstName};
+                sb.AppendLine($"{string.Join(" ",names)} from {e.departmentName} - ${e.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
