4e6f4525f7702a09bccddfb7ae9bb531d6d874eb
MiroslavIvanov8 <litiers3@gmail.com>
2023-10-26 20:40:48 +0300

    i finally got my aha moment !

---
+++ C# DB/Entity Framework Core/02.Entity Framework Introduction/4.Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var contex = new SoftUniContext();
+            //Console.WriteLine(GetEmployeesFullInformation(contex));
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(contex));
+
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary,
+                }).ToList();
+
+            StringBuilder sb = new();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e=>e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e=>e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine, employees
+                .Select(e => $"{e.FirstName} - {e.Salary:f2}"));
+
+            return result.TrimEnd();
+        }
+    }
+}
