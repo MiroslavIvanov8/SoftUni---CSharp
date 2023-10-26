1e48038d13daec19b71fe6c68a32bf14d24b9909
MiroslavIvanov8 <litiers3@gmail.com>
2023-10-26 21:00:38 +0300

    eye of the tiger 05 problem and rocking the basics
    lets see how i am gonna do against update and delete

---
+++ C# DB/Entity Framework Core/02.Entity Framework Introduction/05.Employees from Research and Development/Models/Town.cs
@@ -0,0 +1,17 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni.Models
+{
+    public  class Town
+    {
+        public Town()
+        {
+            Addresses = new HashSet<Address>();
+        }
+
+        public int TownId { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Address> Addresses { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/02.Entity Framework Introduction/05.Employees from Research and Development/StartUp.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var contex = new SoftUniContext();
+            //Console.WriteLine(GetEmployeesFullInformation(contex));
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(contex));
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(contex));
+
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary,
+                }).ToList();
+
+            StringBuilder sb = new();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e=>e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e=>e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine, employees
+                .Select(e => $"{e.FirstName} - {e.Salary:f2}"));
+
+            return result.TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    departmentName = e.Department.Name,
+                    e.Salary
+                }).ToList();
+
+            StringBuilder sb = new();
+            foreach (var e in employees
+                         .OrderBy(e=>e.Salary)
+                         .ThenByDescending(e=>e.FirstName))
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.departmentName} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
