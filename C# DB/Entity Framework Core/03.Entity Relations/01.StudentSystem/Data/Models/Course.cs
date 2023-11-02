5da4187bf05ec3d1ec4d52661ffe4b82c45e20d6
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-02 11:15:42 +0200

    i am starting to get sick of judge

---
+++ C# DB/Entity Framework Core/03.Entity Relations/01.StudentSystem/Data/Models/Course.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            this.StudentsCourses = new HashSet<StudentCourse>();
+            this.Resources = new HashSet<Resource>();
+            this.Homeworks = new HashSet<Homework>();
+        }
+
+        public int CourseId { get; set; }
+
+        public string Name { get; set; }
+        
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public ICollection<StudentCourse> StudentsCourses { get; set; }
+        public ICollection<Resource> Resources { get; set; }
+        public ICollection<Homework> Homeworks { get; set; }
+    }
+}
