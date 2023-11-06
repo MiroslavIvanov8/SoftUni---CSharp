f74e0dc1f64708058c94d6ad7e608216f1f97a25
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-06 19:39:54 +0200

    these problems are kinda fun

---
+++ C# DB/Entity Framework Core/04. LINQ/02. All Albums Produced by Given Producer/Data/Models/Performer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class Performer
+    {
+        public Performer()
+        {
+            this.PerformerSongs = new HashSet<SongPerformer>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(Configuration.PerformerNameMaxLength)]
+        public string FirstName { get; set; }
+        [MaxLength(Configuration.PerformerNameMaxLength)]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal NetWorth { get; set; }
+
+        public virtual ICollection<SongPerformer> PerformerSongs { get; set; }
+    }
+}
