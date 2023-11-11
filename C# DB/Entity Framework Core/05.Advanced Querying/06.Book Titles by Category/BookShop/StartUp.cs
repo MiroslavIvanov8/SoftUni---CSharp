42c45823a0f6d3deb685fccc0619b65f63ebad71
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-11 14:30:28 +0200

    p06 advandec querring
    i need to rethink this problem was quite intriguing

---
+++ C# DB/Entity Framework Core/05.Advanced Querying/06.Book Titles by Category/BookShop/StartUp.cs
@@ -0,0 +1,135 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+using BookShop.Models;
+using BookShop.Models.Enums;
+
+namespace BookShop;
+
+using Data;
+using Initializer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var db = new BookShopContext();
+        DbInitializer.ResetDatabase(db);
+
+        string categories = Console.ReadLine();
+        Console.WriteLine(GetBooksByCategory(db, categories));
+    }
+
+    public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+    {
+        command = command.ToLower();
+        StringBuilder sb = new();
+
+        List<Book> bookInfo = new List<Book>();
+
+        if (command == "minor")
+        {
+            bookInfo = context.Books
+                              .Where(b => b.AgeRestriction == AgeRestriction.Minor)
+                              .OrderBy(b => b.Title).ToList();
+        }
+        else if (command == "teen")
+        {
+            bookInfo = context.Books
+                .Where(b => b.AgeRestriction == AgeRestriction.Teen)
+                .OrderBy(b => b.Title)
+                .ToList();
+
+        }
+        else
+        {
+            bookInfo = context.Books
+                .Where(b => b.AgeRestriction == AgeRestriction.Adult)
+                .OrderBy(b => b.Title)
+                .ToList();
+        }
+
+        foreach (var book in bookInfo)
+        {
+            sb.AppendLine(book.Title);
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public static string GetGoldenBooks(BookShopContext context)
+    {
+        StringBuilder sb = new();
+
+        var goldenBooks = context.Books
+            .Where(b => b.Copies<5000 && b.EditionType == EditionType.Gold)
+            .OrderBy(b => b.BookId);
+
+        foreach (var book in goldenBooks)
+        {
+            sb.AppendLine(book.Title);
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public static string GetBooksByPrice(BookShopContext context)
+    {
+        StringBuilder sb = new();
+
+        var books = context.Books
+            .Select(b => new
+            {
+                b.Title,
+                b.Price
+            })
+            .Where(b => b.Price > 40)
+            .OrderByDescending(b => b.Price);
+            
+
+        foreach(var book in books)
+        {
+            sb.AppendLine($"{book.Title} - ${book.Price:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+    {
+        var books = context.Books
+            .Where(b => b.ReleaseDate.Value.Year != year)
+            .OrderBy(b => b.BookId);
+
+        StringBuilder sb = new();
+        foreach (var book in books)
+        {
+            sb.AppendLine(book.Title);
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public static string GetBooksByCategory(BookShopContext context, string input)
+    {
+        string[] categories = input.ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        var books = context.Books
+            .Where(b => b.BookCategories.Any(y => categories.Contains(y.Category.Name.ToLower())))
+            .Select(b => b.Title)
+            .OrderBy(b => b)
+            .ToList();
+
+        StringBuilder sb = new();
+
+        foreach (var title in books)
+        {
+            sb.AppendLine(title);
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
+
+
+
+
