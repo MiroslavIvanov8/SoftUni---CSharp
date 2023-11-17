9695b32ddb07dc3a09474c5d1b63934b0c333a5f
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-17 21:16:51 +0200

    json exercise
    json exercise p01

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/ProductShop/01. Import Users/ProductShopProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile() 
+        {
+
+            //user
+            this.CreateMap<ImportUserDto, User>();
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/ProductShop/01. Import Users/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+
+            string inputJson = File.ReadAllText(@"../../../Datasets/users.json");
+
+            Console.WriteLine(ImportUsers(dbContext, inputJson));
+        }
+         
+        public static string ImportUsers(ProductShopContext dbContext, string inputJson)
+        {
+            // we create mapper and add specified profile to configure map options
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+
+            //deserialize json file and create dto[] from it
+            ImportUserDto[] userDtos = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+
+            //fill this collection
+            ICollection<User> validUsers = new HashSet<User>();
+            foreach (var userDto in userDtos)
+            {
+                User user = mapper.Map<User>(userDto);
+
+                validUsers.Add(user);
+            }
+
+            // model collection is ready for the db 
+
+            dbContext.AddRange(validUsers);
+
+            dbContext.SaveChanges();
+
+            return $"Successfully imported {validUsers.Count}";
+        }
+    }
+    
+}
