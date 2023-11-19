7edb53298e3e88fc5749069f5384798c7db16f74
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-19 23:32:11 +0200

    uploaded p05 with anonymous type and dtos

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/ProductShop/05. Export Products in Range/Export Products in Range With DTO/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using Z.EntityFramework;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            //string inputJson = File.ReadAllText(@"../../../Datasets/categories-products.json");
+
+            Console.WriteLine(GetProductsInRange(dbContext));
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+                
+            }));
+
+            ExportProductInRangeDto[] productsByRangeDtos = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .AsNoTracking()
+                .ProjectTo<ExportProductInRangeDto>(mapper.ConfigurationProvider)
+                .ToArray();
+          
+          return JsonConvert.SerializeObject(productsByRangeDtos,
+                   Formatting.Indented,
+                   new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }
+          );
+        }
+    }
+    
+}
