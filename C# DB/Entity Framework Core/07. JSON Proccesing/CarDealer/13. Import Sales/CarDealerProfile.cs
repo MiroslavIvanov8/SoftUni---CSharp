992fe92605931376ad9fdd5fc5a093e86df0968a
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 19:58:13 +0200

    p013 sales
    json with automapper

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/13. Import Sales/CarDealerProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForMember(dest => dest.PartsCars, opt 
+                    => opt.MapFrom(src => src.PartsId.Select(partId => new PartCar { PartId = partId }))); ;
+
+            //Sale
+            this.CreateMap<ImportSaleDto, Sale>();
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/13. Import Sales/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText((@"../../../Datasets/sales.json"));
+            Console.WriteLine(ImportSales(dbContext, inputJson));
+
+        }
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+
+            var importCarsDtos = JsonConvert.DeserializeObject<ImportCarDto[]>(inputJson);
+
+            ICollection<Car> validCars = new HashSet<Car>();
+
+            foreach (var carDto in importCarsDtos)
+            {
+                Car car = mapper.Map<Car>(carDto);
+
+                validCars.Add(car);
+            }
+
+            context.AddRange(validCars);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {validCars.Count}.";
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = CamelCaseMapperConfiguration();
+
+            var importSaleDtos = JsonConvert.DeserializeObject<ImportSaleDto[]>(inputJson);
+
+            ICollection<Sale> validSales = new HashSet<Sale>();
+
+            foreach (var dto in importSaleDtos)
+            {
+                Sale sale = mapper.Map<Sale>(dto);
+
+                validSales.Add(sale);
+            }
+
+            context.AddRange(validSales);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {validSales.Count}.";
+        }
+
+        public static IMapper CamelCaseMapperConfiguration() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
