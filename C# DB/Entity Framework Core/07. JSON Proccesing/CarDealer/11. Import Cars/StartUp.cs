6240322a0ff889a7b2010ab261a33d6cd12ce84b
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 18:58:49 +0200

    p11 import cars
    this automapper, thanks chat grp for helping me with this one. Looks like the automapper can be configured to create a mapping table obj whilst only one id of the both keys. Cool way to find a solution for this problem.. haha

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/11. Import Cars/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText((@"../../../Datasets/cars.json"));
+            Console.WriteLine(ImportCars(dbContext, inputJson));
+
+        }
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+
+            var importCarsDtos = JsonConvert.DeserializeObject<ImportCarDto[]>(inputJson);
+
+            ICollection<Car> validCars = new HashSet<Car>();
+
+            foreach (var carDto in importCarsDtos)
+            {
+                Car car = mapper.Map<Car>(carDto);
+
+                validCars.Add(car);
+            }
+
+            context.AddRange(validCars);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {validCars.Count}.";
+        }
+    }
+}
