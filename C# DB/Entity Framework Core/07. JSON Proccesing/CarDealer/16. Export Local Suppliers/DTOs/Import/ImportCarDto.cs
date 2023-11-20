7430e956bd97bf570d100225bdbab3d6fac04792
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 21:31:41 +0200

    p16 json

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/16. Export Local Suppliers/DTOs/Import/ImportCarDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer.DTOs.Import
+{
+    public class ImportCarDto
+    {
+        public ImportCarDto()
+        {
+            this.PartsId = new HashSet<int>();
+        }
+
+        [JsonProperty("make")]
+        public string Make { get; set; } = null!;
+
+        [JsonProperty("model")]
+        public string Model { get; set; } = null!;
+
+        [JsonProperty("traveledDistance")]
+        public int TraveledDistance { get; set; }
+
+        [JsonProperty("partsId")]
+        public ICollection<int> PartsId { get; set; } = null!;
+
+    }
+}
---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/16. Export Local Suppliers/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Castle.Core.Resource;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureCreated();
+
+            //string inputJson = File.ReadAllText((@"../../../Datasets/customers.json"));
+            Console.WriteLine(GetLocalSuppliers(dbContext));
+
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var localSuppliers = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new
+                {
+                    s.Id,
+                    s.Name,
+                    PartsCount = s.Parts.Count
+                })
+                .AsNoTracking()
+                .ToArray();
+
+            return JsonConvert.SerializeObject(localSuppliers, Formatting.Indented);
+        }
+
+        public static IMapper CamelCaseMapperConfiguration() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
