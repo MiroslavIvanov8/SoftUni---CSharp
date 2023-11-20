5d24191e818e167c359c9ac614ac7b68bdc58d46
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 20:07:40 +0200

    p12 json
    inport with automapper dto

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/12. Import Customers/CarDealerProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForMember(dest => dest.PartsCars, opt 
+                    => opt.MapFrom(src => src.PartsId.Select(partId => new PartCar { PartId = partId }))); ;
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>();
+
+            //Sale
+            this.CreateMap<ImportSaleDto, Sale>();
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/12. Import Customers/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Castle.Core.Resource;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText((@"../../../Datasets/customers.json"));
+            Console.WriteLine(ImportCustomers(dbContext, inputJson));
+
+        }
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = CamelCaseMapperConfiguration();
+
+            var importCustomerDtos = JsonConvert.DeserializeObject<ImportCustomerDto[]>(inputJson);
+
+            ICollection<Customer> validCustomers = new HashSet<Customer>();
+
+            foreach (var dto in importCustomerDtos)
+            {
+                Customer customer = mapper.Map<Customer>(dto);
+
+                validCustomers.Add(customer);
+            }   
+
+            context.AddRange(validCustomers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validCustomers.Count}.";
+        }
+
+        public static IMapper CamelCaseMapperConfiguration() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
