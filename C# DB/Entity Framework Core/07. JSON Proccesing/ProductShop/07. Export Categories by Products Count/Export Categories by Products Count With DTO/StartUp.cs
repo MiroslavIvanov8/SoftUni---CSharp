74c8c5828320b67de9f26a0c0bab05a2c546aa94
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 11:56:16 +0200

    p07 json with dto

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/ProductShop/07. Export Categories by Products Count/Export Categories by Products Count With DTO/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Contracts;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using Z.EntityFramework;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            //string inputJson = File.ReadAllText(@"../../../Datasets/categories-products.json");
+
+            Console.WriteLine(GetCategoriesByProductsCount(dbContext));
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+
+            var categoriesByProductCount = context.Categories
+                .OrderByDescending(c => c.CategoriesProducts.Count)
+                .ProjectTo<ExportCategoriesByProductCountDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(categoriesByProductCount,
+                Formatting.Indented,
+                new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        }
+    }
+    
+}
