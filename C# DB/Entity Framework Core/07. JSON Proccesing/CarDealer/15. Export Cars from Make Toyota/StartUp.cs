466db12acdde415814771ed77d07d81623ab9fd6
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 21:02:55 +0200

    p15 json export

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/15. Export Cars from Make Toyota/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Castle.Core.Resource;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureCreated();
+
+            //string inputJson = File.ReadAllText((@"../../../Datasets/customers.json"));
+            Console.WriteLine(GetCarsFromMakeToyota(dbContext));
+
+        }
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            var carsFromToyota = context.Cars
+                .Where(c => c.Make == "Toyota")
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .Select(c => new
+                {
+                    c.Id,
+                    c.Make,
+                    c.Model,
+                    c.TraveledDistance
+                })
+                .AsNoTracking()
+                .ToArray();
+
+            return JsonConvert.SerializeObject(carsFromToyota, Formatting.Indented);
+        }
+
+        public static IMapper CamelCaseMapperConfiguration() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
