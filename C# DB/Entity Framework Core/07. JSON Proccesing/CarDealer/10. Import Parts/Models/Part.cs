fcb50d0c174b7b15d1a27e1e719fb3cc84068a7c
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 17:57:40 +0200

    p10 json
    import parts

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/10. Import Parts/Models/Part.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models
+{
+    public class Part
+    {
+        public Part()
+        {
+            this.PartsCars = new HashSet<PartCar>();
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!; 
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [ForeignKey(nameof(Supplier))]
+        public int SupplierId { get; set; }
+        public virtual Supplier Supplier { get; set; } = null!;
+
+        public virtual ICollection<PartCar> PartsCars { get; set; } 
+    }
+}
---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/10. Import Parts/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText((@"../../../Datasets/parts.json"));
+            Console.WriteLine(ImportParts(dbContext, inputJson));
+
+        }
+
+        public static string ImportParts(CarDealerContext dbContext, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+
+            var importPartDtos = JsonConvert.DeserializeObject<ImportPartDto[]>(inputJson);
+
+            ICollection<Part> validParts = new HashSet<Part>();
+
+            foreach (var dto in importPartDtos)
+            {
+                if (!dbContext.Suppliers.Any(s => s.Id == dto.SupplierId))
+                {
+                    continue;
+                }
+
+                Part part = mapper.Map<Part>(dto);
+
+                validParts.Add(part);
+            }
+
+            dbContext.BulkInsert(validParts);
+
+            return $"Successfully imported {validParts.Count}.";
+        }
+    }
+}
