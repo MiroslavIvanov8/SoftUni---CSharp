977e95176ed780d90440a4a308c87bcfe78a5064
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 11:13:06 +0200

    p06 json with anonymous obj

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/ProductShop/06. Export Sold Products/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.Contracts;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using Z.EntityFramework;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            //string inputJson = File.ReadAllText(@"../../../Datasets/categories-products.json");
+
+            Console.WriteLine(GetSoldProducts(dbContext));
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var usersWithSoldProducts = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.Buyer != null))
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Select(u => new
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    SoldProducts = u.ProductsSold.Where(p => p.Buyer != null)
+                        .Select(p => new
+                        {
+                            Name = p.Name,
+                            Price = p.Price,
+                            BuyerFirstName = p.Buyer.FirstName,
+                            BuyerLastName = p.Buyer.LastName,
+                        })
+                })
+                .AsNoTracking()
+                .ToArray();
+
+            return JsonConvert.SerializeObject(usersWithSoldProducts,
+                Formatting.Indented,
+                new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        }
+    }
+    
+}
