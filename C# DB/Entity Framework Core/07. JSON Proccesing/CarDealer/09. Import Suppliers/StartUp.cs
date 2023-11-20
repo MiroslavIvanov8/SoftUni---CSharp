97fc3aef7a614ba343db05d13572aa76a1511589
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 16:28:59 +0200

    p09 json import suppliers
    with import Dto and automapper

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContex.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText((@"../../../Datasets/suppliers.json"));
+            Console.WriteLine(ImportSuppliers(dbContext, inputJson));
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext dbContext, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+
+            var importSuppliersDtos = JsonConvert.DeserializeObject<ImportSupplierDto[]>(inputJson);
+
+            ICollection<Supplier> validDtos = new HashSet<Supplier>();
+
+            foreach (var dto in importSuppliersDtos)
+            {
+                Supplier supplier = mapper.Map<Supplier>(dto);
+
+                validDtos.Add(supplier);
+            }
+
+            dbContext.BulkInsert(validDtos);
+
+            return $"Successfully imported {validDtos.Count}.";
+        }
+    }
+}
