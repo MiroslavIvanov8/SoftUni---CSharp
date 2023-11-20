ea11c585283e162e2196499ea73c129361f3ceac
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-20 11:55:26 +0200

    p07 with anonymous object

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/ProductShop/07. Export Categories by Products Count/Export Categories by Products Count With Anonymoust Object/ProductShopProfile.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile() 
+        {
+
+            //user
+            this.CreateMap<ImportUserDto, User>();
+
+            //product
+            this.CreateMap<ImportProductDto, Product>();
+
+            this.CreateMap<Product, ExportProductInRangeDto>()
+                .ForMember(d => d.ProductName,
+                    opt => opt.MapFrom(s => s.Name))
+                .ForMember(d => d.ProductPrice,
+                    opt => opt.MapFrom(s => s.Price))
+                .ForMember(d => d.ProductSeller,
+                    opt => opt.MapFrom(s => $"{s.Seller.FirstName} {s.Seller.LastName}"));
+
+            //category
+            this.CreateMap<ImportCategoryDto, Category>();
+
+            //categoryProduct
+            this.CreateMap<ImportCategoryProductDto, CategoryProduct>();
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/ProductShop/07. Export Categories by Products Count/Export Categories by Products Count With Anonymoust Object/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.Contracts;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using Z.EntityFramework;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            //string inputJson = File.ReadAllText(@"../../../Datasets/categories-products.json");
+
+            Console.WriteLine(GetCategoriesByProductsCount(dbContext));
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categoriesByProductCount = context.Categories
+                .OrderByDescending(c => c.CategoriesProducts.Count)
+                .Select(c => new
+                {
+                    Category = c.Name,
+                    ProductsCount = c.CategoriesProducts.Count,
+                    AveragePrice = c.CategoriesProducts.Average(p => p.Product.Price).ToString("f2"),
+                    TotalRevenue = c.CategoriesProducts.Sum(p => p.Product.Price).ToString("f2")
+                })
+                .ToArray();
+
+            return JsonConvert.SerializeObject(categoriesByProductCount,
+                Formatting.Indented,
+                new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        }
+    }
+    
+}
