d7e6a571d127b74c268d27f0def6f86ae2880884
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-21 10:45:27 +0200

    о17 йзон
    the console was not able to put the whole result in so i really spend few hours trying to figure out why i get wrong answers. Also i found a different way to structure json file obj : arr,
    and another obj: arr

---
+++ C# DB/Entity Framework Core/07. JSON Proccesing/CarDealer/17. Export Cars with Their List of Parts/StartUp.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Castle.Core.Resource;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+using Z.BulkOperations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureCreated();
+
+            //string inputJson = File.ReadAllText((@"../../../Datasets/customers.json"));
+            Console.WriteLine(GetCarsWithTheirListOfParts(dbContext));
+
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var localSuppliers = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new
+                {
+                    s.Id,
+                    s.Name,
+                    PartsCount = s.Parts.Count
+                })
+                .AsNoTracking()
+                .ToArray();
+
+            return JsonConvert.SerializeObject(localSuppliers, Formatting.Indented);
+        }
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Select(c => new
+                {
+                    car = new
+                    {
+                        Make = c.Make,
+                        Model = c.Model,
+                        TraveledDistance = c.TraveledDistance
+                    },
+                    parts = c.PartsCars.Select(p => new
+                    {
+                        Name = p.Part.Name,
+                        Price = p.Part.Price.ToString("f2")
+                    })
+                })
+                .ToArray();
+
+            ;
+            return JsonConvert.SerializeObject(cars, Formatting.Indented);
+        }
+
+        public static IMapper CamelCaseMapperConfiguration() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
