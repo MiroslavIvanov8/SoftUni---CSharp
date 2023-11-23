25752c32bbd406175d0974e15a1e2f6512ce0d3e
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-23 19:15:49 +0200

    p15 xml export with attribute

---
+++ C# DB/Entity Framework Core/08. XML Processing/CarDealer/15. Export Cars from Make BMW/CarDealerProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Cars
+            /*this.CreateMap<ImportCarDto, Car>()
+                .ForMember(d => d.PartsCars, opt => 
+                    opt.MapFrom(s => s.Parts
+                       .Select(p => new PartCar(){ PartId = p.PartId})));*/
+
+            this.CreateMap<ImportCarDto, Car>()
+                .ForSourceMember(s => s.Parts, opt => opt.DoNotValidate());
+
+            this.CreateMap<Car, ExportCarDto>();
+            this.CreateMap<Car, ExportCarAttributeDto>();
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>();
+
+            //Sale
+            this.CreateMap<ImportSaleDto, Sale>();
+        }
+    }
+}
