1268c32a1064a25f453fd94a97814b1fa2ada2b4
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-23 17:27:49 +0200

    xml p11 car import
    i am gonna lie down and start crying

---
+++ C# DB/Entity Framework Core/08. XML Processing/CarDealer/11. Import Cars/StartUp.cs
@@ -0,0 +1,126 @@
+﻿using System.IO;
+using System.Linq.Expressions;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using ProductShop.Utilities;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText("../../../Datasets/cars.xml");
+            Console.WriteLine(ImportCars(dbContext, inputXml));
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] importSupplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+            
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+
+            foreach (var supplierDto in importSupplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue; 
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+
+                validSuppliers.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(validSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count}";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportPartDto[] importPartDtos = xmlHelper.Deserialize<ImportPartDto[]>(inputXml, "Parts");
+
+            ICollection<Part> validParts =  new HashSet<Part>();
+
+            foreach (var partDto in importPartDtos)
+            {
+                if (!context.Suppliers.Any(s => s.Id == partDto.SupplierId))
+                {
+                    continue;
+                }
+
+                Part part = mapper.Map<Part>(partDto);
+
+                validParts.Add(part);
+            }
+
+            context.Parts.AddRange(validParts);
+            context.SaveChanges();
+
+            return $"Successfully imported {validParts.Count}";
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportCarDto[] carsDtos = xmlHelper.Deserialize<ImportCarDto[]>(inputXml, "Cars");
+
+            ICollection<Car> validCars = new HashSet<Car>();
+            foreach (ImportCarDto carDto in carsDtos)
+            {
+                if (string.IsNullOrEmpty(carDto.Make) || string.IsNullOrEmpty(carDto.Model))
+                {
+                    continue;
+                }
+
+                Car car = mapper.Map<Car>(carDto);
+
+                
+                foreach (var partDto in carDto.Parts.DistinctBy(p => p.PartId))
+                {
+                    if (!context.Parts.Any(p => p.Id == partDto.PartId))
+                    {
+                        continue;
+                    }
+
+                    PartCar partCar = new PartCar()
+                    {
+                        PartId = partDto.PartId
+                    };
+
+                    car.PartsCars.Add(partCar); 
+                }
+
+                validCars.Add(car);
+
+            }
+
+            context.Cars.AddRange(validCars);
+            context.SaveChanges();
+
+            return $"Successfully imported {validCars.Count}";
+        }
+
+        private static IMapper CreateMapper() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
---
+++ C# DB/Entity Framework Core/08. XML Processing/CarDealer/11. Import Cars/Utilities/XmlHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Utilities;
+
+public class XmlHelper
+{
+    public T Deserialize<T>(string inputXml, string rootName)
+    {
+        XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+        using StringReader reader = new StringReader(inputXml);
+
+        T deserializedObj = (T)xmlSerializer.Deserialize(reader);
+
+        return deserializedObj;
+    }
+
+    public T[] DeserializeCollection<T>(string inputXml, string rootName)
+    {
+        XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRoot);
+
+        using StringReader reader = new StringReader(inputXml);
+        T[] deserializedObj = (T[])xmlSerializer.Deserialize(reader);
+
+        return deserializedObj;
+    }
+
+}
