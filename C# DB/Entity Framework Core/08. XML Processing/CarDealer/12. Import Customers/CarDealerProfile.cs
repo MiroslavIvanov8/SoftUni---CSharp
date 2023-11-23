fa4e7c9d6c2447cb726e59c5974301befaebb482
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-23 18:14:41 +0200

    import without nested arrays are easy

---
+++ C# DB/Entity Framework Core/08. XML Processing/CarDealer/12. Import Customers/CarDealerProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Cars
+            /*this.CreateMap<ImportCarDto, Car>()
+                .ForMember(d => d.PartsCars, opt => 
+                    opt.MapFrom(s => s.Parts
+                       .Select(p => new PartCar(){ PartId = p.PartId})));*/
+
+            this.CreateMap<ImportCarDto, Car>()
+                .ForSourceMember(s => s.Parts, opt => opt.DoNotValidate());
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>();
+        }
+    }
+}
