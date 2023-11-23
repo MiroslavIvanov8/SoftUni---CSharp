1a5405960b725763775727fddac28b2a2d1b21c0
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-23 13:44:58 +0200

    xml sucks
    i dont like xml than much !

---
+++ C# DB/Entity Framework Core/08. XML Processing/CarDealer/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using ProductShop.Utilities;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            Console.WriteLine(ImportSuppliers(dbContext, inputXml));
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] importSupplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+            
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+
+            foreach (var supplierDto in importSupplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue; 
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+
+                validSuppliers.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(validSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count}";
+        }
+
+        private static IMapper CreateMapper() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
---
+++ C# DB/Entity Framework Core/08. XML Processing/CarDealer/10. Import Parts/StartUp.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Linq.Expressions;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using ProductShop.Utilities;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText("../../../Datasets/parts.xml");
+            Console.WriteLine(ImportParts(dbContext, inputXml));
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] importSupplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+            
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+
+            foreach (var supplierDto in importSupplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue; 
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+
+                validSuppliers.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(validSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count}";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportPartDto[] importPartDtos = xmlHelper.Deserialize<ImportPartDto[]>(inputXml, "Parts");
+
+            ICollection<Part> validParts =  new HashSet<Part>();
+
+            foreach (var partDto in importPartDtos)
+            {
+                if (!context.Suppliers.Any(s => s.Id == partDto.SupplierId))
+                {
+                    continue;
+                }
+
+                Part part = mapper.Map<Part>(partDto);
+
+                validParts.Add(part);
+            }
+
+            context.Parts.AddRange(validParts);
+            context.SaveChanges();
+
+            return $"Successfully imported {validParts.Count}";
+        }
+
+        private static IMapper CreateMapper() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
