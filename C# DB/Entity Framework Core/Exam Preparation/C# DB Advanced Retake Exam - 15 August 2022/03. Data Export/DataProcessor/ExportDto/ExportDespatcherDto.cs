e1ed1ed0f970efa2935d750b65710cb55bd8ad5a
MiroslavIvanov8 <litiers3@gmail.com>
2023-11-27 17:25:15 +0200

    finished

---
+++ C# DB/Entity Framework Core/Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/03. Data Export/DataProcessor/ExportDto/ExportDespatcherDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    [XmlType("Despatcher")]
+    public class ExportDespatcherDto
+    {
+        [XmlAttribute("TrucksCount")]
+        public int Count { get; set; }
+
+        [XmlElement("DespatcherName")]
+        public string DespatcherName { get; set; } = null!;
+
+        [XmlArray("Trucks")]
+        public ExportTruckDto[] Trucks { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/03. Data Export/DataProcessor/Serializer.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using Trucks.DataProcessor.ExportDto;
+using Trucks.Utilities;
+
+namespace Trucks.DataProcessor
+{
+    using Data;
+    using Trucks.Data.Models.Enums;
+
+    public class Serializer
+    {
+        
+        public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportDespatcherDto[] despatcherDtos = context.Despatchers
+                .Where(d => d.Trucks.Any())
+                .Select(d => new ExportDespatcherDto()
+                {
+                    DespatcherName = d.Name,
+                    Count = d.Trucks.Count,
+                    Trucks = d.Trucks.Select(t => new ExportTruckDto()
+                        {
+                            RegistrationNumber = t.RegistrationNumber,
+                            Make = t.MakeType.ToString()
+                        })
+                        .OrderBy(t => t.RegistrationNumber)
+                        .ToArray()
+                })
+                .OrderByDescending(d => d.Count)
+                .ThenBy(d => d.DespatcherName)
+                .ToArray();
+
+            return xmlHelper.Serialize(despatcherDtos, "Despatchers");
+        }
+
+        public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
+        {
+            var clients = context.Clients
+                .ToArray()
+                .Where(c => c.ClientsTrucks.Any(ct => ct.Truck.TankCapacity >= capacity))
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    Trucks = c.ClientsTrucks.Where(ct => ct.Truck.TankCapacity >= capacity)
+                        .Select(t => new
+                        {
+                            TruckRegistrationNumber = t.Truck.RegistrationNumber,
+                            VinNumber = t.Truck.VinNumber,
+                            TankCapacity = t.Truck.TankCapacity,
+                            CargoCapacity = t.Truck.CargoCapacity,
+                            CategoryType = t.Truck.CategoryType.ToString(),
+                            MakeType = t.Truck.MakeType.ToString()
+                        })
+                        .OrderBy(t => t.MakeType)
+                        .ThenByDescending(t => t.CargoCapacity)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.Trucks.Length)
+                .ThenBy(c => c.Name)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(clients, Formatting.Indented);
+        }
+    }
+}
