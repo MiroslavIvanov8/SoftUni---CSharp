c46da488fb423b5a435bfe6ab64c1e4b64247cbf
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-05-03 22:35:30 +0300

    Last one and going to bed
    Will put more time in those, those are pretty fun. Interesting problems to solve.

---
+++ Data Structures/Data Structures Fundamentals/Linear Data Structures Exercise/05. Reverse Numbers with a Stack/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05._Reverse_Numbers_with_a_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
