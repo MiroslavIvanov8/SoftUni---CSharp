84986b3bf07c4465228d11b7a9ffe31c7df4a45e
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-05-04 13:28:29 +0300

    Linear Structures Problems finished.
    pretty interesting problems. Will prolly get back later to do them again

---
+++ Data Structures/Data Structures Fundamentals/Linear Data Structures Exercise/06.  Calculate Sequence with a Queue/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _06.__Calculate_Sequence_with_a_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Queue<int> queue = new Queue<int>();
+            Queue<int> deqQueue = new Queue<int>();
+
+            queue.Enqueue(number);
+            List<int> numbers = new List<int>();
+
+            numbers.Add(number);
+
+            for (int i = number ; i < 18; i++)
+            {
+                int number1 = queue.Dequeue();
+                int number2 = number1 + 1;
+                int number3 =  2 * number1 + 1;
+                int number4 = number1  + 2;
+
+                queue.Enqueue(number2);
+                queue.Enqueue(number3);
+                queue.Enqueue(number4);
+
+                numbers.Add(number2);
+                numbers.Add(number3);
+                numbers.Add(number4);
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+            
+        }
+    }
+}
---
+++ Data Structures/Data Structures Fundamentals/Linear Data Structures Exercise/07. Sequence N  M/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _07._Sequence_N___M
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = numbers[0];
+            int m = numbers[1];
+
+            Queue<Item<int>> queue = new Queue<Item<int>>();
+            List<int> result = new List<int>();
+            Item<int> item = new Item<int>(n); 
+
+            queue.Enqueue(item);
+
+            while (queue.Any())
+            {   
+                item = queue.Dequeue();
+                if (item.Value < m)
+                {
+                    queue.Enqueue(new Item<int>((item.Value + 1), item));
+                    queue.Enqueue(new Item<int>((item.Value + 2), item));
+                    queue.Enqueue(new Item<int>((item.Value * 2), item));
+
+                }
+                else if(item.Value == m)
+                {
+                    while (item != null)
+                    {
+                        result.Add(item.Value);
+                        item = item.Previous;
+                    }
+
+                    result.Reverse(0, result.Count);
+                    Console.WriteLine(string.Join(" -> ", result));
+                    return;
+                }
+
+            }
+        }
+
+        class Item<T>
+        {
+            public Item(T value, Item<T> previous = null)
+            {
+                this.Value = value;
+                this.Previous = previous;
+            }
+            public T Value { get; set; }
+            public Item<T> Previous { get; set; }
+        }
+    }
+}
