685b0912c65991a2a3c3249ef2e17381020739f0
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-05-10 00:43:07 +0300

    Completed BFS and DFS lab

---
+++ Data Structures/Data Structures Fundamentals/03.Trees Representation and Traversal (BFS-DFS) Lab/02. Breadth First Order/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tree = new Tree<int>(34);
+            tree.AddChild(34,new Tree<int>(1));
+            tree.AddChild(1, new Tree<int>(2));
+
+            tree.RemoveNode(34);
+
+        }
+    }
+}
