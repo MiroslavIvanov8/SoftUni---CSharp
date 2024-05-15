bfc813a6fab67d82cd4358bcb2c882d141180f89
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-05-15 22:13:47 +0300

    BTS and Heaps lab Data Strutures

---
+++ Data Structures/Data Structures Fundamentals/05.Heaps BST Lab/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+
+namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T element, IAbstractBinaryTree<T> left, IAbstractBinaryTree<T> right)
+        {
+            this.Value = element;
+            this.LeftChild = left;
+            this.RightChild = right;
+        }
+
+        public T Value { get; private set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            StringBuilder sb = new StringBuilder();
+            this.PreOrderDfs(sb, indent, this);
+
+            return sb.ToString();
+        }
+
+        private void PreOrderDfs(StringBuilder sb, int indent, IAbstractBinaryTree<T> binaryTree)
+        {
+            sb.Append(new string(' ', indent)).AppendLine(binaryTree.Value.ToString());
+
+            if (binaryTree.LeftChild != null)
+            {
+                this.PreOrderDfs(sb,indent + 2, binaryTree.LeftChild);
+            }
+
+            if (binaryTree.RightChild != null)
+            {
+                this.PreOrderDfs(sb, indent + 2, binaryTree.RightChild);
+            }
+        }
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            this.ForEachInOrder(action, this);
+        }
+
+        private void ForEachInOrder(Action<T> action, IAbstractBinaryTree<T> binaryTree)
+        {
+            if (binaryTree == null)
+            {
+                return;
+            }
+            
+            this.ForEachInOrder(action, binaryTree.LeftChild);
+
+            action.Invoke(binaryTree.Value);
+
+            this.ForEachInOrder(action, binaryTree.RightChild);
+
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> InOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.InOrder());
+            }
+
+            result.Add(this);
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.InOrder());
+            }
+            return result;
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PostOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PostOrder());
+            }
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PostOrder());
+            }
+
+            result.Add(this);
+
+            return result;
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PreOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+
+            result.Add(this);
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PreOrder());
+            }
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PreOrder());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Data Structures/Data Structures Fundamentals/05.Heaps BST Lab/03.MaxHeap/MaxHeap.cs
@@ -0,0 +1,127 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _03.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
+    {
+        private List<T> elements;
+
+        public MaxHeap()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Size => this.elements.Count;
+
+        public void Add(T element)
+        {
+           this.elements.Add(element);
+           this.HeapifyUp(this.elements.Count -1);
+        }
+
+        private void HeapifyUp(int index)
+        {
+            int parentIndex = this.GetParentIndex(index);
+            while (index >= 0 && this.IsGreater(index, parentIndex))
+            {
+                this.Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = this.GetParentIndex(index);
+            }
+        }
+
+        private void Swap(int index, int parentIndex)
+        {
+            var element = this.elements[index];
+            this.elements[index] = this.elements[parentIndex];
+            this.elements[parentIndex] = element;
+        }
+
+        private bool IsGreater(int index, int parentIndex)
+        {
+            return this.elements[index].CompareTo(this.elements[parentIndex]) > 0;
+        }
+
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        public T ExtractMax()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var element = this.elements[0];
+            this.Swap(0, this.elements.Count - 1);
+            this.elements.RemoveAt(this.elements.Count - 1);
+            HeapifyDown(0);
+
+            return element;
+        }
+
+        private void HeapifyDown(int index)
+        {
+            int biggerChildIndex = this.GetBiggerChildIndex(index);
+            while (IsIndexValid(biggerChildIndex) && this.IsGreater(biggerChildIndex, index))
+            {
+                this.Swap(index, biggerChildIndex);
+
+                index = biggerChildIndex;
+                biggerChildIndex = GetBiggerChildIndex(index);
+            }
+        }
+
+
+
+        private bool IsIndexValid(int index)
+        {
+            return index >= 0 && index < this.elements.Count;
+        }
+
+        private int GetBiggerChildIndex(int index)
+        {
+            int leftChildIndex = (index * 2) + 1;
+            int rightChildIndex = (index * 2) + 2;
+
+
+            //interesting part !!!
+            if (rightChildIndex < this.elements.Count)
+            {
+                if (this.elements[leftChildIndex].CompareTo(this.elements[rightChildIndex]) > 0)
+                {
+                    return leftChildIndex;
+                }
+
+                return rightChildIndex;
+            }
+            else if (leftChildIndex < this.elements.Count)
+            {
+                return leftChildIndex;
+            }
+            else
+            {
+
+                return -1;
+            }
+
+
+            return default;
+        }
+
+        public T Peek()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.elements[0];
+        }
+    }
+}
