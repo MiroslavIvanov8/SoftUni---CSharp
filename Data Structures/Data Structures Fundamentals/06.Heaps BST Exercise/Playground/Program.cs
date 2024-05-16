fa3dcb21ce52f030586da355c56adc624fa0fd85
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-05-16 13:16:52 +0300

    BTS and Heaps Exercise started

---
+++ Data Structures/Data Structures Fundamentals/06.Heaps BST Exercise/Playground/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using _02.BinarySearchTree;
+using _03.MinHeap;
+using _04.CookiesProblem;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBinarySearchTree<int> bts = new BinarySearchTree<int>();
+
+            bts.Insert(10);
+            bts.Insert(20);
+            bts.Insert(30);
+            bts.Insert(11);
+            
+
+            bts.DeleteMin();
+        }
+    }
+}
