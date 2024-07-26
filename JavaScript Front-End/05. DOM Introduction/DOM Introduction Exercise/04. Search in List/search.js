73730f7dde075dfeb81427a6551940858dca75d7
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-07-26 22:36:43 +0300

    05 Dom Introcution
    p04. Search in List. Lacking time to do all the problems and watch the exercise. Hope i manage to do all my necessary work so i keep it with the studies. DOM seems pretty awesome.

--- JavaScript Front-End/05. DOM Introduction/DOM Introduction Exercise/04. Search in List/search.js
+++ JavaScript Front-End/05. DOM Introduction/DOM Introduction Exercise/04. Search in List/search.js
@@ -1,3 +1,32 @@
 function search() {
-   // TODO
+   const towns = document.getElementById('towns').textContent.split('\n');
+   let townsElements = document.getElementsByTagName('li');
+   const searchTerm = document.getElementById('searchText').value.toLowerCase();
+ 
+   debugger;
+   let townsElementsArr = Array.from(townsElements);
+   let resultElement = document.getElementById('result');
+
+   for (let i = 0; i < townsElementsArr.length; i++) {
+      townsElements[i].style.fontWeight = 300;
+      townsElements[i].style.textDecoration = 'none';
+   }
+
+   const townsArr = Array.from(towns)
+   .filter(town => town.trim().toLowerCase().includes(searchTerm));
+
+   for (let i = 0; i < townsElementsArr.length; i++) {
+      let currTown = townsElements[i].textContent
+
+      for (let j = 0; j < townsArr.length; j++) {
+         const matchTown = townsArr[j].trim();;
+
+         if (currTown == matchTown) {
+            townsElements[i].style.fontWeight = 'bold';
+            townsElements[i].style.textDecoration = 'underline';
+         }
+      }
+   }    
+   
+   resultElement.textContent = `${townsArr.length} matches found`;
 }
