27d450881bd9dc9995704937c59b18ab821f067c
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-08-03 23:24:22 +0300

    06.Dom events and manipu
    p01

--- JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/01. Sections/script.js
+++ JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+
+   const divResultEl = document.querySelector('div#content');
+   words.forEach((word) => {
+      const divEl = document.createElement('div');
+      const paragraphEl = document.createElement('p');
+      
+      paragraphEl.textContent = word;
+      paragraphEl.style.display = 'none';     
+
+      divEl.appendChild(paragraphEl);
+      divResultEl.appendChild(divEl);
+   })
+
+   divResultEl.addEventListener('click', (e) => {      
+      if(e.target.tagName == 'DIV'){
+         e.target.children[0].style.display = 'block';
+      }
+   })
 }
