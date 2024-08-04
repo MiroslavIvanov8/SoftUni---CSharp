64b527f741d41d23f0af42eff4cf77a98d99efd8
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-08-04 18:27:28 +0300

    06 Dom Manip and Events Exercise
    p 03, 04

--- JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/03. Locked-Profile/app.js
+++ JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,36 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    //Add event listener to show more button
+    const showMoreBtns = document.querySelectorAll('button');
+
+    function showMoreClickHandler(button) {
+        button.addEventListener('click', (e) => {
+
+            const divProfile = e.target.parentElement;
+            const checkedRadioBtn = [...divProfile.querySelectorAll('input[type=radio]')]
+                .filter((btn) => btn.checked);
+                            
+            if (checkedRadioBtn[0].value === 'lock') {
+                return;
+            } else {
+                const [email, age] = e.target.parentElement.querySelectorAll('input[type=email]');
+
+                const hiddenDivEl = email.parentElement;
+               
+                
+                if (hiddenDivEl.style.display === 'none' ||
+                    hiddenDivEl.style.display === ""
+                ) {
+                    hiddenDivEl.style.display = 'block';
+                    e.target.textContent = 'Hide it';
+                } else {
+                    hiddenDivEl.style.display = 'none';
+                    e.target.textContent = 'Show more';
+                }
+            }
+        })
+    }
+
+    showMoreBtns.forEach((button) => showMoreClickHandler(button));
+
 }
--- JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/04. Fill-Dropdown/dropdown.js
+++ JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,20 @@
 function addItem() {
-    console.log('TODO:...');
+    
+    //Get newItemText and newItemValue values
+    const selectMenuEl = document.getElementById('menu');
+    const newItemTextEl = document.getElementById('newItemText');
+    const newItemValueEl = document.getElementById('newItemValue');    
+
+    //Create new options object with given values
+    const optionEl = document.createElement('option');
+    optionEl.text = newItemTextEl.value;
+    optionEl.value = newItemValueEl.value;
+
+    //Append it to select object with id menu
+    selectMenuEl.appendChild(optionEl);
+
+    //Clear their values
+    newItemTextEl.value = '';
+    newItemValueEl.value = '';
+
 }
