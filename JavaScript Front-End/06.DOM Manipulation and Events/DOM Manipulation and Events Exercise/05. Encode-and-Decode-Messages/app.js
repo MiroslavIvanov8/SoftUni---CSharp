017343d83891128a6e39683caa6a6295b829f1b0
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-08-05 21:17:12 +0300

    06.Dom Mani And Events
    p 3-4-5-6

--- JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/05. Encode-and-Decode-Messages/app.js
+++ JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    
+    //Get buttons and attach eventHandlers to them
+    const [sendBtn, readBtn] = document.querySelectorAll('button');
+    const sendTextareaEl = sendBtn.parentElement.querySelector('textarea');
+    const receiveTextareaEl = readBtn.parentElement.querySelector('textarea');
+    
+    sendBtn.addEventListener('click', (e) => {       
+        
+        //Get the text from the first textarea, encode it 
+        const message = Array.from(sendTextareaEl.value).reduce((acc, curr) => {
+            return acc += String.fromCharCode(curr.charCodeAt(curr) + 1);
+        },"")
+        
+        //and send it to the second textarea, clear first one    
+        sendTextareaEl.value = '';
+        receiveTextareaEl.value = message;     
+    })     
+
+    readBtn.addEventListener('click', (e) => {
+        
+        //Decode the text from the second textarea
+        const decodedMessage = Array.from(receiveTextareaEl.value).reduce((acc, curr) => {
+            return acc += String.fromCharCode(curr.charCodeAt(curr) - 1);
+        },"")
+        
+        receiveTextareaEl.value = decodedMessage;
+    })
+    
 }
--- JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/06. Furniture/solution.js
+++ JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/06. Furniture/solution.js
@@ -1,4 +1,64 @@
 function solve() {
 
-  //TODO...
+  //Get Generate button and add event listener to him
+  const [generateBtn, buyBtn] = document.querySelectorAll('button');
+  const tbodyEl = document.querySelector('.table tbody');
+  const firstTrEl = document.querySelector('.table tbody tr');
+  const [inputTextarea, outputTextarea] = document.querySelectorAll('textarea');
+
+  function appendTrForEachData({ img, name, price, decFactor }) {
+    const currTrCone = firstTrEl.cloneNode(true);
+
+    currTrCone.children[0].children[0].setAttribute('src', img);
+    currTrCone.children[0].innerHtml = currTrCone.children[0].innerHtml.trim();
+
+    currTrCone.children[1].children[0].textContent = name;
+    currTrCone.children[1].innerHtml = currTrCone.children[1].innerHtml.trim();
+
+    currTrCone.children[2].children[0].textContent = price;
+    currTrCone.children[2].innerHtml = currTrCone.children[2].innerHtml.trim();
+
+    currTrCone.children[3].children[0].textContent = decFactor;
+    currTrCone.children[3].innerHtml = currTrCone.children[3].innerHtml.trim();
+
+    const checkboxEl = currTrCone.children[4].children[0];
+    currTrCone.children[4].innerHtml = currTrCone.children[4].innerHtml.trim();
+    
+    checkboxEl.removeAttribute('disabled');
+    checkboxEl.setAttribute('name', name);
+    checkboxEl.setAttribute('price', price);
+    checkboxEl.setAttribute('decFactor', decFactor);
+
+    tbodyEl.appendChild(currTrCone);
+  }
+
+  function onGenerateBtnClickHandler() {
+    const inputData = JSON.parse(inputTextarea.value);
+
+    inputData.forEach(appendTrForEachData)
+  }
+
+  function onBuyBtnClickHandler() {
+    const outputData = [...document.querySelectorAll('input[type=checkbox]')]
+      .filter((el) => el.checked).reduce((acc, currInputEl) => {
+
+        const name = currInputEl.getAttribute('name');
+        const price = currInputEl.getAttribute('price');
+        const decFactor = currInputEl.getAttribute('decFactor');
+
+        acc.names.push(name);
+        acc.totalPrice += Number(price);
+        acc.totalDecFactor += Number(decFactor);
+        return acc;
+      }, { names: [], totalPrice: 0, totalDecFactor: 0 });
+
+    outputTextarea.value = `Bought furniture: ${outputData.names.join(', ')}\nTotal price: ${outputData.totalPrice.toFixed(2)}\nAverage decoration factor: ${(outputData.decFactor / outputData.names.length).toFixed(2)}`
+  }
+
+  generateBtn.addEventListener('click', onGenerateBtnClickHandler)
+
+  buyBtn.addEventListener('click', onBuyBtnClickHandler)
+
+  //Get Buy button and add event listener to him
+
 }
