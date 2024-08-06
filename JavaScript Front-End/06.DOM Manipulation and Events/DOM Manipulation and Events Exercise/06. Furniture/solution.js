72ced99a601ceff843442d056533c39a1f177250
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-08-06 20:30:30 +0300

    06.Dom Mani Events
    Gosh solved funiture all by myself

--- JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/06. Furniture/solution.js
+++ JavaScript Front-End/06.DOM Manipulation and Events/DOM Manipulation and Events Exercise/06. Furniture/solution.js
@@ -1,64 +1,83 @@
 function solve() {
 
-  //Get Generate button and add event listener to him
-  const [generateBtn, buyBtn] = document.querySelectorAll('button');
-  const tbodyEl = document.querySelector('.table tbody');
-  const firstTrEl = document.querySelector('.table tbody tr');
-  const [inputTextarea, outputTextarea] = document.querySelectorAll('textarea');
-
-  function appendTrForEachData({ img, name, price, decFactor }) {
-    const currTrCone = firstTrEl.cloneNode(true);
-
-    currTrCone.children[0].children[0].setAttribute('src', img);
-    currTrCone.children[0].innerHtml = currTrCone.children[0].innerHtml.trim();
-
-    currTrCone.children[1].children[0].textContent = name;
-    currTrCone.children[1].innerHtml = currTrCone.children[1].innerHtml.trim();
-
-    currTrCone.children[2].children[0].textContent = price;
-    currTrCone.children[2].innerHtml = currTrCone.children[2].innerHtml.trim();
-
-    currTrCone.children[3].children[0].textContent = decFactor;
-    currTrCone.children[3].innerHtml = currTrCone.children[3].innerHtml.trim();
-
-    const checkboxEl = currTrCone.children[4].children[0];
-    currTrCone.children[4].innerHtml = currTrCone.children[4].innerHtml.trim();
+    //Get generate and buy buttons
+    const [generateBtn, buyBtn] = document.querySelectorAll('button');
+    const [inputTextareaEl, outputTextareaEl] = document.querySelectorAll('textarea');
+    const tableRowEl = document.querySelector('tbody');
     
-    checkboxEl.removeAttribute('disabled');
-    checkboxEl.setAttribute('name', name);
-    checkboxEl.setAttribute('price', price);
-    checkboxEl.setAttribute('decFactor', decFactor);
+    //Attach eventlistener to generate button
+    function generateFurniture(e) {    
+          
+      //Parse the data from the array and create dom row object
+      const inputData = JSON.parse(inputTextareaEl.value);
+      inputData.forEach((obj) => {
+        const trEl = document.createElement('tr');
 
-    tbodyEl.appendChild(currTrCone);
+        //create img td
+        const tdImg = document.createElement('td');
+        const imgEl = document.createElement('img');
+        imgEl.src = obj.img;
+        tdImg.appendChild(imgEl);
+        trEl.appendChild(tdImg);
+
+        //create tdName
+        const tdName = document.createElement('td');
+        const pNameEl = document.createElement('p');
+        pNameEl.textContent = obj.name;
+        tdName.appendChild(pNameEl);
+        trEl.appendChild(tdName);
+
+        //create tdPrice
+        const tdPrice = document.createElement('td');
+        const pPriceEl = document.createElement('p');
+        pPriceEl.textContent = obj.price;
+        tdPrice.appendChild(pPriceEl);
+        trEl.appendChild(tdPrice);
+
+        //create tdDecFactor
+        const tdDecFactor = document.createElement('td');
+        const pDecFactorEl = document.createElement('p');
+        pDecFactorEl.textContent = obj.decFactor;
+        tdDecFactor.appendChild(pDecFactorEl);
+        trEl.appendChild(tdDecFactor);
+
+        
+        //create tdCheckbox
+        const tdCheckBox = document.createElement('td');
+        const inputCheckBoxEl  = document.createElement('input');
+        inputCheckBoxEl.setAttribute('type', 'checkbox');
+        tdCheckBox.appendChild(inputCheckBoxEl);
+        trEl.appendChild(tdCheckBox);
+
+        tableRowEl.appendChild(trEl);
+
+      })
+      
   }
-
-  function onGenerateBtnClickHandler() {
-    const inputData = JSON.parse(inputTextarea.value);
-
-    inputData.forEach(appendTrForEachData)
+    
+    generateBtn.addEventListener('click', generateFurniture)
+    
+    //Attach eventlistener to buy button
+    buyBtn.addEventListener('click', (e) => {
+      
+      //Get all checked items
+        const checked = [...document.querySelectorAll('input[type=checkbox]')]
+        .filter((e) => e.checked);
+        
+        //Output
+        const bought = checked.map((input) => input.closest('tr').children[1].children[0].textContent);
+        const totalPrice = checked.reduce((acc, curr) => {
+            const price = Number(curr.closest('tr').children[2].children[0].textContent);
+          return acc += price;
+        }, 0)
+        
+        const avgDecFactor = (checked.reduce((acc, curr) => {
+              const decFactor = Number(curr.closest('tr').children[3].children[0].textContent);
+              return acc += decFactor;
+            }, 0)) / checked.length;
+        
+        outputTextareaEl.value = `Bought furniture: ${bought.join(', ')}\nTotal price: ${totalPrice.toFixed(2)}\nAverage decoration factor: ${avgDecFactor}`;
+    })
+    
   }
-
-  function onBuyBtnClickHandler() {
-    const outputData = [...document.querySelectorAll('input[type=checkbox]')]
-      .filter((el) => el.checked).reduce((acc, currInputEl) => {
-
-        const name = currInputEl.getAttribute('name');
-        const price = currInputEl.getAttribute('price');
-        const decFactor = currInputEl.getAttribute('decFactor');
-
-        acc.names.push(name);
-        acc.totalPrice += Number(price);
-        acc.totalDecFactor += Number(decFactor);
-        return acc;
-      }, { names: [], totalPrice: 0, totalDecFactor: 0 });
-
-    outputTextarea.value = `Bought furniture: ${outputData.names.join(', ')}\nTotal price: ${outputData.totalPrice.toFixed(2)}\nAverage decoration factor: ${(outputData.decFactor / outputData.names.length).toFixed(2)}`
-  }
-
-  generateBtn.addEventListener('click', onGenerateBtnClickHandler)
-
-  buyBtn.addEventListener('click', onBuyBtnClickHandler)
-
-  //Get Buy button and add event listener to him
-
-}
+  
